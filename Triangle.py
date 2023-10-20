d48448abc4bc6f62fd11bf7463ced68081e3772c
SaraIranian <148322008+SaraIranian@users.noreply.github.com>
2023-10-20 12:54:07 +0330

    Add files via upload

--- Triangle.py
+++ Triangle.py
@@ -2,9 +2,7 @@ a=int(input("Enter a number for a"))
 b=int(input("Enter a number for b"))
 c=int(input("enter a number for c"))
 
-if a+c>b:
-    if a+b>c:
-        if b+c>a:
-            print("There is a triangle with these numbers")
+if a+c>b and a+b>c and b+c>a:
+    print("There is a triangle with these numbers")
 else:
     print("You can not draw a triangle with these numbers")
